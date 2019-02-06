508d7200f1c148d7df3ffc1636969871a39a0178
josephdanciu <jd2279@nau.edu>
2019-02-06 14:47:01 -0700

    Added a test top bar with our fake logo that we can change it later

--- homepage.php
+++ homepage.php
@@ -6,16 +6,37 @@ echo "This is the homepage";
 ?>
 
 <html>
-    <head>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     
-    </head>
+    <link rel = "stylesheet"
+    type = "text/css"
+    href = "homepage.css" />
+</head>
     
-    <body>
     
+<body>
+
+<div class="navbar">
+    <a href="#home">
+  <img src="logo%20copy.jpg" alt="Logo" class = "logo picture" style="opacity: 0.7; width: 19px;"></a>
+    
+  <a href="#home">Home</a>
+  <a href="#songs">Songs</a>
+  <a href="#shop">Shop</a>
+  <a href="#discover">Discover</a>
+    <a href="#account" style="float: right">
+        Account
+    <img src="logo%20copy.jpg" alt="Logo" class = "logo picture" style="opacity: 0.7; width: 20px; float: left">
+    
+    </a>
+</div>
+
+<div class="main">
     <form action="payment.php">
     <input type="submit" value="Go to payment" />
 </form>
+</div>
     
-    </body>
-
+</body>
 </html>
