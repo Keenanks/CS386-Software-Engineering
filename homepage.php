401699501b1aef16b5a9e343b77d32936c26ce1e
Keenanks <kks259@nau.edu>
2019-02-06 10:59:19 -0700

    Added button that goes to payment.php for now

--- homepage.php
+++ homepage.php
@@ -1,3 +1,21 @@
+<!DOCTYPE html>
 <?php
 echo "This is the homepage";
+
+
 ?>
+
+<html>
+    <head>
+    
+    </head>
+    
+    <body>
+    
+    <form action="payment.php">
+    <input type="submit" value="Go to payment" />
+</form>
+    
+    </body>
+
+</html>
