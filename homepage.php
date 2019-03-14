614fdebe7230edf122f505274551e45d9a3beae5
Keenanks <kks259@nau.edu>
2019-03-13 22:52:58 -0700

    Tray's fire songs

--- homepage.php
+++ homepage.php
@@ -14,47 +14,14 @@
     
     
 <body>
-
-<div class="navbar">
-    <a href="#home">
-  <img src="logo%20copy.jpg" alt="Logo" class = "logo picture" style="opacity: 0.7; width: 16px;"></a>
     
-  <a href="#home">Home</a>
-  <a href="#songs">Songs</a>
-  <a href="#shop">Shop</a>
-  <a href="#discover">Discover</a>
-  <div class="dropdown">
-  <button class="dropbtn" onclick="myFunction()">Account
-  </button>
-  <div class="dropdown-content" id="myDropdown">
-    <a href="#">Link 1</a>
-    <a href="#">Link 2</a>
-    <a href="#">Link 3</a>
-  </div>
-  </div>
-</div>
+    <?php 
     
-<script>
-/* When the user clicks on the button, 
-toggle between hiding and showing the dropdown content */
-function myFunction() {
-  document.getElementById("myDropdown").classList.toggle("show");
-}
-
-// Close the dropdown if the user clicks outside of it
-window.onclick = function(e) {
-  if (!e.target.matches('.dropbtn')) {
-  var myDropdown = document.getElementById("myDropdown");
-    if (myDropdown.classList.contains('show')) {
-      myDropdown.classList.remove('show');
-    }
-  }
-}
-</script>
-
-<div class="main">
-    <h1>HOMEPAGE BITCHES</h1>
-</div>
+    /* This links the NavBar to the current page */
+        include ( 'NavBar.php' );
+    ?>
+    
+    <h1>HOMEPAGE</h1>
     
 </body>
 </html>
