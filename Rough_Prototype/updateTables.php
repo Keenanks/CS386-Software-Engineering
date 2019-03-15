093325ad657cc5ba88a1f28867674352d9c58feb
Keenanks <kks259@nau.edu>
2019-03-14 19:00:11 -0700

    updated method of adding songs to be... can now check if a same song has been added by same artist

--- Rough_Prototype/updateTables.php
+++ Rough_Prototype/updateTables.php
@@ -18,39 +18,77 @@ if( !$conn )
   die( "Connection failed: " . mysqli_connect_error() );
  }
 
-$songList = "INSERT INTO songTable (song_file, album, genre, artist, title) VALUES ('Dee_Yan-Key_-_03_-_Intermezzo_Spettrale.mp3', 'Divertimento of Life', 'Classical',
-'Dee Yan-Key', 'Intermezzo: Spettrale');";
 
-$songList .= "INSERT INTO songTable (song_file, album, genre, artist, title) VALUES ('Dee_Yan-Key_-_04_-_Vivace_con_spirito.mp3', 'Divertimento of Life', 'Classical',
-'Dee Yan-Key', 'Vivace con spirito');";
-
-$songList .= "INSERT INTO songTable (song_file, album, genre, artist, title) VALUES ('Deep.mp3', '386SQUAD', 'HipHop',
-'NOR.T.H', 'Deep');";
-
-$songList .= "INSERT INTO songTable (song_file, album, genre, artist, title) VALUES ('Ocean.mp3', '386SQUAD', 'HipHop',
-'NOR.T.H', 'Ocean');";
-
-$songList .= "INSERT INTO songTable (song_file, album, genre, artist, title) VALUES ('Peak.mp3', '386SQUAD', 'HipHop',
-'NOR.T.H', 'Peak');";
-
-$songList .= "INSERT INTO songTable (song_file, album, genre, artist, title) VALUES ('Potential.mp3', '386SQUAD', 'HipHop',
-'NOR.T.H', 'Potential');";
-
-$songList .= "INSERT INTO songTable (song_file, album, genre, artist, title) VALUES ('Reality.mp3', '386SQUAD', 'HipHop',
-'NOR.T.H', 'Reality');";
-
-$songList .= "INSERT INTO songTable (song_file, album, genre, artist, title) VALUES ('Unchanged.mp3', '386SQUAD', 'HipHop',
-'NOR.T.H', 'Unchanged');";
-
-if( mysqli_multi_query($conn, $songList))
+// check if song exists in Database
+function checkSong($song_title, $artist)
    {
-    echo "songs added successfully"; 
+    global $conn;
+    
+    // sql code selecting songs that have the same title and author
+    $sql = "SELECT * FROM songTable WHERE title = '".$song_title."' AND artist = '".$artist."';";
+    
+    // checks the database for the sql code
+    $tableCheck = mysqli_query($conn, $sql);
+    
+    // variable with number of rows as a result of selecting
+    $songCount = mysqli_num_rows($tableCheck);
+    
+    // if count is greater than one that means the song with that artist is already in database
+    //   else we return false because it is not in the database.
+    if($songCount > 0)
+    {
+        return true;
+    }
+    return false;
    }
-else
+
+
+
+// create function that adds song table
+function placeSongIntoTable($file, $album_name, $genre, $artist, $song_title)
    {
-    echo "Error: " . $songList . "<br>" . mysqli_error($conn);
+    global $conn;
+    
+    // sql string for adding song
+    $songString =  "INSERT INTO songTable (song_file, album, genre, artist, title) VALUES ('".$file.
+        "', '".$album_name."', '".$genre."','".$artist."', '".$song_title."');";
+    
+    // check if song is in database
+    if( checkSong( $song_title, $artist) )
+       {
+        // tells user the song is in the database already
+        echo $song_title." by ".$artist." already exists.";
+        echo "<br><br>";
+       }
+    
+    // if valid add song to table
+    elseif( mysqli_query($conn, $songString))
+      {
+       // tells user if song was successfully added
+       echo $song_title. " by ".$artist." added successfully.";
+       echo "<br><br>";
+      }
+    else
+      {
+       echo "Error: " . $songString . "<br>" . mysqli_error($conn);
+       echo "<br><br>";
+      }
    }
+
+placeSongIntoTable('Unchanged.mp3','386SQUAD','HipHop','NOR.T.H', 'Unchanged');
+
+placeSongIntoTable('Ocean.mp3','386SQUAD','HipHop','NOR.T.H', 'Ocean');
+
+placeSongIntoTable('Reality.mp3','386SQUAD','HipHop','NOR.T.H', 'Reality');
+
+placeSongIntoTable('Potential.mp3','386SQUAD','HipHop','NOR.T.H', 'Potential');
+
+placeSongIntoTable('Peak.mp3','386SQUAD','HipHop','NOR.T.H', 'Peak');
+
+placeSongIntoTable('Deep.mp3','386SQUAD','HipHop','NOR.T.H', 'Deep');
+
 
 mysqli_close($conn);
+
 
 ?>
