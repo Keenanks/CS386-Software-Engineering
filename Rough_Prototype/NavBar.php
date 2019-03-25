675008fd1eaadf3f9bddd9f7d82b153c3434e548
Keenanks <kks259@nau.edu>
2019-03-25 09:25:26 -0700

    added on click functions to navigation

--- Rough_Prototype/NavBar.php
+++ Rough_Prototype/NavBar.php
@@ -6,35 +6,46 @@
 		<meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<link rel="stylesheet" href ="homepage.css" />
-		<title>Nav-Bar</title>
 		
+        <script>
+        
+            function updateTime()
+            {
+                sessionStorage.setItem('timeStamp',audio.currentTime);
+            }
+            
+            
+        
+        
+        
+        </script>
 	</head>
 		
 		
-	<body>
+	<body onbeforeunload="return updateTime()">
 		<nav>
-			<div class="logo">
-				<a href="homepage.php">
-						<img border="0" alt="Stream" src="stream-logo.png" width="100" height="100">
+			<div class="logo" id = 'logoHome'>
+				<a  href="homepage.php" onclick="updateTime()">
+						<img  border="0" alt="Stream" src="stream-logo.png" width="100" height="100">
 				</a>
 			</div>
 			
 			<ul class="nav-buttons">
 				
 				<li>
-					<a href="songpage.php">Music</a>
+					<a href="songpage.php" onclick="updateTime()">Music</a>
 				</li>
 				
 				<li>
-					<a href="#shop">Shop</a>
+					<a href="#shop" onclick="updateTime()">Shop</a>
 				</li>
 				
 				<li>
-					<a href="#discover">Discover</a>
+					<a href="#discover" onclick="updateTime()">Discover</a>
 				</li>
 				
 				<li>
-					<a href="login.html">User</a>
+					<a href="login.html" onclick="updateTime()">User</a>
 				</li>
 			</ul>
 			
@@ -49,6 +60,11 @@
 		</nav>
 
 		<script src="NavBar_script.js"></script>
+        
+        <?php include('audioBar.php');?>
+        
 		
 	</body>
+    
+    
 </html>
